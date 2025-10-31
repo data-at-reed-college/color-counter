9a29ad3587cd2d3146bd58698ad21fe534831b17
Leland-Russell <172640492+Leland-Russell@users.noreply.github.com>
2025-10-31 10:39:48 -0700

    Respodning to feedback

--- working_color_counter.py
+++ working_color_counter.py
@@ -12,9 +12,9 @@ green_prob_start = 0.8
 blue_prob_start = 0.8
 green_goal_start = 20
 blue_goal_start = 20
+max_turns_start = 20
 
-sidebar_state = "closed" # start sidebar closed by default; a possibly better alternative would be
-# sidebar_state = "desktop"
+sidebar_state = "desktop" 
 
 green_hex="#34b754"
 blue_hex="#337ab7"
@@ -36,7 +36,7 @@ class Data: # Simple object to store and later write data
             if newline:
                 s += "\n"
             s += str(row[0]) # so we can get commas in an easy way
-            i = 1
+            i = 1 
             while i < self.ncol:
                 s += "," + str(row[i])
                 i += 1
@@ -49,7 +49,7 @@ class Data: # Simple object to store and later write data
         self.data = []
     # I probably should have just used pandas but I forgot about that
 
-data_cols = ["green_score", "blue_score", "button_chosen", "green_prob", "blue_prob", "gren_goal", "blue_goal", "green_start", "blue_start"] # columns to collect data on. Really the last 4 are largely fixed values, but there's no other way to record them nicely in a csv
+data_cols = ["green_score", "blue_score", "button_chosen", "number_of_turns","green_goal", "blue_goal", "green_start", "blue_start"] # columns to collect data on. Really the last 4 are largely fixed values, but there's no other way to record them nicely in a csv
 
 app_ui = ui.page_auto(
     ui.sidebar(
@@ -57,10 +57,10 @@ app_ui = ui.page_auto(
         ui.p(description), 
         ui.download_button("save_data", "Save Data", class_="btn-secondary"), # to save data
         ui.h1("Adjust parameters:"), # these are largely self explanatory
-        ui.input_numeric("green_prob_chooser", "Probability of green button working", 
-                         0.8, min=0, max=1),
-        ui.input_numeric("blue_prob_chooser", "Probability of blue button working", 
-                         0.8, min=0, max=1),
+#        ui.input_numeric("green_prob_chooser", "Probability of green button working", 
+#                         0.8, min=0, max=1),
+#        ui.input_numeric("blue_prob_chooser", "Probability of blue button working", 
+#                         0.8, min=0, max=1),
         ui.input_numeric("green_win_condition", "Points for green completion", 
                          20, min=0, max=512),
         ui.input_numeric("blue_win_condition", "Points for blue completion", 
@@ -69,6 +69,8 @@ app_ui = ui.page_auto(
                          0, min=0, max=512),
         ui.input_numeric("blue_start", "Points to start blue at", 
                          0, min=0, max=512),
+        ui.input_numeric("max_turns", "Number of button presses",
+                         20, min=0, max=512),
         ui.input_action_button("update_params", "Update Parameters"),
         width = 450,
         open=sidebar_state
@@ -78,10 +80,14 @@ app_ui = ui.page_auto(
             ui.input_action_button("green_btn", "Pick Green!", class_="btn-lg",
                                    style = f"height: 125px; color: #fff; background-color: {green_hex};"),
             ui.layout_columns(
-                ui.input_action_button("reset", "Reset Scores", 
+                ui.input_action_button("reset", "Reset Scores\n(resets data)",
                                        class_="btn-secondary", style = "height: 125px; width = 125px"),
-                ui.input_action_button("reset_data", "Reset Data",
-                                       class_="btn-secondary", style = "height: 125px; width = 125px")
+#                ui.input_action_button("reset_data", "Reset Data",
+#                                       class_="btn-secondary", style = "height: 125px; width = 125px")
+                ui.card(
+                    ui.h5("Turns left:"),
+                    ui.output_text("turns_left")
+                    )
             ),
             ui.input_action_button("blue_btn", "Pick Blue!", class_="btn-lg",
                                    style = f"height: 125px; color: #fff; background-color: {blue_hex};")
@@ -120,13 +126,14 @@ def server(input, output, session):
     count1 = reactive.value(0) # could be changed to better name
     count2 = reactive.value(0)
     green_prob = reactive.value(green_prob_start) # various internal parameters
-    blue_prob = reactive.value(blue_prob_start)
+    blue_prob = reactive.value(blue_prob_start) # these prob parameters aren't being used, so if we were spending more time, it would be worth removing them and replacing all with hardcoded values, but whatever
     green_goal = reactive.value(green_goal_start)
     blue_goal = reactive.value(blue_goal_start)
     green_start = reactive.value(0)
     blue_start = reactive.value(0)
     green_sign = reactive.value(1) # these are based on start and goals, tell whether or not to decrement or increment scores; take on either 1 or -1
     blue_sign = reactive.value(1)
+    num_turns = reactive.value(max_turns_start)
 
     data = reactive.value(Data(data_cols)) 
     # data stores a pointer to a Data object. If we want to modify data, we modify the pointer in data.get() as a Data object. This makes the data local to each web-session, so the data don't mingle with each other
@@ -135,23 +142,29 @@ def server(input, output, session):
     @reactive.effect
     @reactive.event(input.green_btn) 
     def _():
-        data.get().append([count1(), count2(), "'Green'", green_prob(), blue_prob(), 
+        if num_turns() <= 0:
+            return
+        data.get().append([count1(), count2(), "'Green'", num_turns(),
                     green_goal(), blue_goal(), green_start(), blue_start()]) # I don't know what data they want
         if random.random() <= green_prob(): # could increment either counters
             count1.set(count1() + green_sign() * 1)
-            return
-        count2.set(count2() + blue_sign() * 1)
+        if random.random() > green_prob(): # probability to increment the other, I'm using green_prob just because we want the two probabilities to be 0.8 and 0.2 and 0.2 happens to be 1 - 0.8
+            count2.set(count2() + blue_sign() * 1)
+        num_turns.set(num_turns()-1)
 
     
     @reactive.effect # Now for the blue button
     @reactive.event(input.blue_btn)
     def _():
-        data.get().append([count1(), count2(), "'Blue'", green_prob(), blue_prob(), 
+        if num_turns() <= 0:
+            return
+        data.get().append([count1(), count2(), "'Blue'", num_turns(),
                     green_goal(), blue_goal(), green_start(), blue_start()]) # I don't know what data they want
         if random.random() <= blue_prob():
             count2.set(count2() + blue_sign() * 1)
-            return
-        count1.set(count1() + green_sign() * 1)
+        if random.random() > blue_prob(): # same comment as earlier
+            count1.set(count1() + green_sign() * 1)
+        num_turns.set(num_turns()-1)
     
     # Reset both counters when reset button is pressed; do we want this?
     @reactive.effect
@@ -159,22 +172,24 @@ def server(input, output, session):
     def _():
         count1.set(0)
         count2.set(0)
+        num_turns.set(input.max_turns())
 
-    @reactive.effect
-    @reactive.event(input.reset_data)
-    def _():
-        data.get().clear_data()
+#    @reactive.effect
+#    @reactive.event(input.reset_data)
+#    def _():
+#        data.get().clear_data()
 
 
     @reactive.effect
     @reactive.event(input.update_params)
     def _():
-        green_prob.set(input.green_prob_chooser())
-        blue_prob.set(input.blue_prob_chooser())
+#        green_prob.set(input.green_prob_chooser())
+#        blue_prob.set(input.blue_prob_chooser())
         green_goal.set(input.green_win_condition())
         blue_goal.set(input.blue_win_condition())
         green_start.set(input.green_start())
         blue_start.set(input.blue_start())
+        num_turns.set(input.max_turns())
         count1.set(green_start())
         count2.set(blue_start())
 
@@ -201,6 +216,9 @@ def server(input, output, session):
     @render.text
     def blue_prob_text():
         return "The probability of modifying blue with a blue button press is " + str(blue_prob())
+    @render.text
+    def turns_left():
+        return str(num_turns())
     @render.plot
     def plot():
         k = 0 # Make bars aligned well
